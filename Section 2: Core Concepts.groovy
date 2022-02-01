45677c6530a1e610e2dfce0d30260b18f4638f7d
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-02-01 21:55:30 +0000

    Update Section 2: Core Concepts.groovy

--- Section 2: Core Concepts.groovy
+++ Section 2: Core Concepts.groovy
@@ -270,8 +270,7 @@ Labels and Selectors:
 - Delete the two newly created ReplicaSets - replicaset-1 and replicaset-2 = kubectl delete replicasets -n default replicaset-1
 - Fix the original replica set new-replica-set to use the correct busybox image = kubectl edit replicaset -n default new-replica-set followed by deleting the pods and allowing it to recreate.
 - Scale the ReplicaSet to 5 PODs. Use kubectl scale command or edit the replicaset using kubectl edit replicaset. kubectl edit replicaset -n default new-replica-set, delete pods and let it start recreate again
-  kubectl scale --replica=6 -f new-replica-set
-
+  kubectl scale rs new-replica-set --replicas=5
   
 
  
