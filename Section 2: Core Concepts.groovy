1a81ac41bf5ce22be5c09b62bf850af8f9cbe507
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-02-07 22:37:00 +0000

    Update Section 2: Core Concepts.groovy

--- Section 2: Core Concepts.groovy
+++ Section 2: Core Concepts.groovy
@@ -337,4 +337,18 @@ selector:
    matchLabels:
       type: front-end
 
+Deployments test: Lab 3
 
+- How many pods exist in the default namespace? 0 
+- How many ReplicaSets exist on the system? 0 
+- How many Deployments exist on the system? 0
+- How many Deployments exist on the system now? 1
+- How many ReplicaSets exist on the system now? 1
+- How many PODs exist on the system now? 4
+- Out of all the existing PODs, how many are ready? 0
+- What is the image used to create the pods in the new deployment? busybox888
+- Why do you think the deployment is not ready? image doesnt exist
+- Create a new Deployment using the deployment-definition-1.yaml file located at /root/ kubectl create -f deployment-definition-1.yaml, need to edit kind field: should mention capital D in Deployment.
+- Create a new Deployment with the below attributes using your own deployment definition file: kubectl create deployment httpd-frontend --image=httpd:2.4-alpine --replicas=3 --dry-run=client -o yaml > image-deployment.yml
+kubectl create -f image-deployment.yml
+- 
