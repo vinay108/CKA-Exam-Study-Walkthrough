d6a043b6b16f6987dc70c1f2959cc6e2c2f5d214
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-02-12 19:25:06 +0000

    Update Section 2: Core Concepts.groovy

--- Section 2: Core Concepts.groovy
+++ Section 2: Core Concepts.groovy
@@ -577,6 +577,10 @@ Imperative challenge:
 - Create a service redis-service to expose the redis application within the cluster on port 6379, kubectl expose pods redis --port=6379 --name=redis-service
 - Create a deployment named webapp using the image kodekloud/webapp-color with 3 replicas. kubectl create  deployment webapp --image=kodekloud/webapp-color --replicas=3
 - Create a new pod called custom-nginx using the nginx image and expose it on container port 8080.  kubectl run custom-nginx --image=nginx --port=8080
+- Create a new namespace called dev-ns: kubectl create namespace dev-ns
+- Create a new deployment called redis-deploy in the dev-ns namespace with the redis image. It should have 2 replicas = kubectl create deployment redis-deploy -n dev-ns --image=redis --replicas=2
+- Create a pod called httpd using the image httpd:alpine in the default namespace. Next, create a service of type ClusterIP by the same name (httpd). The target port for the service should be 80.
+     - kubectl run httpd --image=httpd:alpine --port=80 --expose
 
  
  
