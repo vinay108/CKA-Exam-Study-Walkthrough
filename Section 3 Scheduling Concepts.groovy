6f4a3b512941964c12a5e79b01726a85bb5fbb9e
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-03-08 21:37:22 +0000

    Update Section 3 Scheduling Concepts.groovy

--- Section 3 Scheduling Concepts.groovy
+++ Section 3 Scheduling Concepts.groovy
@@ -1,5 +1,7 @@
+Manually scheduling:
+     
 - Kube Scheduler, schedule pods to nodes
-- every pod has a field called node name which isnt set when you create pod menifest file, k8s adds.
+- every pod has a field called node name which is not set when you create pod menifest file, k8s adds this automatically.
 - Kube schedule looks at all the pods which do not have the 'node name' property set.
 - If a pod does not have a node name set in the property, it schedule pods to a node. 
 - if there is no scheduler you would need to manually assign pods to node yourself
@@ -37,4 +39,17 @@ spec:
      name: nginx
   nodeName: controlplane
 ~                                                                                                                                                               
-~                                         
+~               
+     
+Labels and Selectors:
+     
+- Are used to identify where objects belong to
+- for each object attach labels as per your needs i.e app / function
+- specify condition to filer out objects
+- In pod definition file, you can specify under metadata section called labels and add as many labels as you like. 
+- once pod is created you can select pod with specific labels using this command kubectl get pods --selector app=App1
+- For example to create a replicaset consisting of 3 different pods you can labels the pod definition and use label the pod definition - 
+  and group the pods by adding the spec.
+- Need to be aware that there are 2 sections with labels
+     - the top part where it specifies the label, is the label of the replicaset, the labels under the template section is the labels of the pods. 
+apiVersion:
