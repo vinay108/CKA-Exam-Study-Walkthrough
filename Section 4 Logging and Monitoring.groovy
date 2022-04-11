5fbd24cb63fff16f5ec90cb4c209a564cd266f72
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-04-11 18:30:48 +0100

    Update Section 4 Logging and Monitoring.groovy

--- Section 4 Logging and Monitoring.groovy
+++ Section 4 Logging and Monitoring.groovy
@@ -1,1 +1,16 @@
+Monitoring K8s:
+  - Node/Pod level metric
+  - disk
+  - cpu
+  - Mem
+  - Networking
 
+- Open source solution avaiable as k8s does not come with one, have to use 3rd party ones such as:
+  - Prometheus
+  - Elastic Stack
+  - DataDog 
+  - Dynatrace
+  - Heapster was used for monitoring however this was deprecated and now it is Metric Server
+   
+- Metric server, recieves metrics from each of the k8s nodes/pods and stores them in memory (in-memory) and does not store metrics on disk and cannot see historical 
+  data. 
