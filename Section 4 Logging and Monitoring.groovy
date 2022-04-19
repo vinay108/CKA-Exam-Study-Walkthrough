70ed33748e4148cc38625ccdb361c202243860ac
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-04-19 17:24:15 +0100

    Update Section 4 Logging and Monitoring.groovy

--- Section 4 Logging and Monitoring.groovy
+++ Section 4 Logging and Monitoring.groovy
@@ -14,3 +14,7 @@ Monitoring K8s:
    
 - Metric server, recieves metrics from each of the k8s nodes/pods and stores them in memory (in-memory) and does not store metrics on disk and cannot see historical 
   data. 
+
+- In the kubelet it has something called a 'cAdvisor' which is used to retrieve metrics from pods exposing them via kubelet API to make them avaiable. 
+- Kubectl top node
+- kubeclt top pod
