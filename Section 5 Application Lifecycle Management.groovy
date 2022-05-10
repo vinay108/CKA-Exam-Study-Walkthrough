9596570a6f19349ae4f6483086a132887738945c
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-05-10 21:41:34 +0100

    Update Section 5 Application Lifecycle Management.groovy

--- Section 5 Application Lifecycle Management.groovy
+++ Section 5 Application Lifecycle Management.groovy
@@ -51,3 +51,11 @@ spec:
                      kubectl rollout history deployment/myapp-deployment
    rollback -------- kubectl rollout undo deployment/myapp-deploy
   
+Practice test: Managing application logs:
+  
+-  We have deployed a POD hosting an application. Inspect it. Wait for it to start.
+- A user - USER5 - has expressed concerns accessing the application. Identify the cause of the issue.
+  Inspect the logs of the POD = kubectl logs webapp-1
+- We have deployed a new POD - webapp-2 - hosting an application. Inspect it. Wait for it to start.
+- A user is reporting issues while trying to purchase an item. Identify the user and the cause of the issue.
+  Inspect the logs of the webapp in the POD = Since there is more then 1 container, need to specify the container name = kubectl logs webapp-2 -c simple-webapp
