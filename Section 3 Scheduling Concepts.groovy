b4454f4530ebe1ead554cb3aa45e49a67bde48e5
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-09-09 19:41:22 +0100

    Update Section 3 Scheduling Concepts.groovy

--- Section 3 Scheduling Concepts.groovy
+++ Section 3 Scheduling Concepts.groovy
@@ -778,12 +778,66 @@ Cluster roles:
  3. How many ClusterRoleBindings exist on the cluster? = 54 kubectl get clusterrolebindings --no-headers | wc -l
  4. What namespace is the cluster-admin clusterrole part of?  = Cluster roles are not based on namespaced and cluster wide
  5. What user/groups are the cluster-admin role bound to? = ubectl get clusterrolebindings cluster-admin -o wide = system:masters
- 6. What level of permission does the cluster-admin role grant? = 
- 7. 
- 8. 
+ 6. What level of permission does the cluster-admin role grant? = perform any action on the resource
+ 7. A new user michelle joined the team. She will be focusing on the nodes in the cluster. 
+    Create the required ClusterRoles and ClusterRoleBindings so she gets access to the nodes.
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: node-admin
+rules:
+- apiGroups: [""]
+  resources: ["nodes"]
+  verbs: ["get", "watch", "list", "create", "delete"]
 
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: michelle-binding
+subjects:
+- kind: User
+  name: michelle
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: node-admin
+  apiGroup: rbac.authorization.k8s.io
 
+8. michelle's responsibilities are growing and now she will be responsible for storage as well. 
+   Create the required ClusterRoles and ClusterRoleBindings to allow her access to Storage.
+   Get the API groups and resource names from command kubectl api-resources. Use the given spec
+   = 
+9. michelle's responsibilities are growing and now she will be responsible for storage as well. Create the required ClusterRoles and ClusterRoleBindings to allow her access to Storage.
+   Get the API groups and resource names from command kubectl api-resources. Use the given spec:
+   = 
+        ---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: storage-admin
+rules:
+- apiGroups: [""]
+  resources: ["persistentvolumes"]
+  verbs: ["get", "watch", "list", "create", "delete"]
+- apiGroups: ["storage.k8s.io"]
+  resources: ["storageclasses"]
+  verbs: ["get", "watch", "list", "create", "delete"]
 
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: michelle-storage-admin
+subjects:
+- kind: User
+  name: michelle
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: storage-admin
+  apiGroup: rbac.authorization.k8s.io
        
      
 
